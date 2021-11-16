ab454e41aa7b0143330165c34430d77c296652ef
Mårten Olsson <marten.olsson@edu.newton.se>
2021-11-16 14:46:20 +0100

    Category pages

--- WebshopProject/Pages/Categories/Womens_wear.cshtml.cs
+++ WebshopProject/Pages/Categories/Womens_wear.cshtml.cs
@@ -1,40 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebshopProject.Pages
+namespace WebshopProject.Pages.Categories
 {
-    public class IndexModel : PageModel
+    public class WomensModel : PageModel
     {
-        
         public IEnumerable<Models.Product> GetProducts { get; set; }
 
         [BindProperty]
         public string Search { get; set; }
-
         public void OnGet()
         {
             GetProducts = (IEnumerable<Models.Product>)Data.ApiManager.GetProducts();
-
+            GetProducts = GetProducts.Where(m => m.category.ToLower().Contains("women's clothing"));
         }
         public void OnPost()
         {
             GetProducts = (IEnumerable<Models.Product>)Data.ApiManager.GetProducts();
-            if(Search != null)
+            GetProducts = GetProducts.Where(m => m.category.ToLower().Contains("women's clothing"));
+            if (Search != null)
             {
                 string search = Search.ToLower();
                 GetProducts = GetProducts.Where(m => m.title.ToLower().Contains(search));
             }
-
-           
-          
         }
     }
 }
