3a5143553872b2af08a7e19e3f6630eb166de3fd
Mårten Olsson <marten.olsson@edu.newton.se>
2021-11-17 16:31:36 +0100

    Databas

---
+++ WebshopProject/Migrations/20211117152706_Cartel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebshopProject.Migrations
+{
+    public partial class Cartel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rating",
+                columns: table => new
+                {
+                    rate = table.Column<float>(type: "real", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rating", x => x.rate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    price = table.Column<float>(type: "real", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ratingrate = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Product_Rating_ratingrate",
+                        column: x => x.ratingrate,
+                        principalTable: "Rating",
+                        principalColumn: "rate",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ratingrate",
+                table: "Product",
+                column: "ratingrate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Rating");
+        }
+    }
+}
