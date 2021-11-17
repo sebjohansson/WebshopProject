8e0b553c55e2cd4cec859ec228c4ca465eb8e64d
sebastian.erik.johansson <sebastian.erik.johansson@gmail.com>
2021-11-17 13:42:44 +0100

    Moved Admin pages to new admin folder.

--- WebshopProject/Models/Product.cs
+++ WebshopProject/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace WebshopProject.Models
 {
     public class Product
     {
+        [Key]
         public int id { get; set; }
         public string title { get; set; }
         public float price { get; set; }
